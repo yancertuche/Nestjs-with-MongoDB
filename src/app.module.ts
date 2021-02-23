4bd20fb2a2cdd6a8dfe1dab308fc2dc507f2b234
Yan <yancertuche@gmail.com>
2021-02-23 16:25:33 -0500

    adding services files

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksController } from './tasks/tasks.controller';
+import { TasksService } from './tasks/tasks.service';
 
 @Module({
   imports: [],
   controllers: [AppController, TasksController],
-  providers: [AppService],
+  providers: [AppService, TasksService],
 })
 export class AppModule {}
