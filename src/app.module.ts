3b936c9ab16e829bb26eeb86f1925d19bbd62bf4
Yan <yancertuche@gmail.com>
2021-02-23 16:28:23 -0500

    adding task module file

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksController } from './tasks/tasks.controller';
 import { TasksService } from './tasks/tasks.service';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [],
+  imports: [TasksModule],
   controllers: [AppController, TasksController],
   providers: [AppService, TasksService],
 })
